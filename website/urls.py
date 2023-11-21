3a0135c5483274844b65179f154fdbf6ad7d3ba6
mobin <6ix.mobin@gmail.com>
2023-11-21 17:54:32 +0330

    added static directories and urls in settings
    +static root
    +static url
    +media root
    +media url

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from website import views
 
 urlpatterns = [
     path('', views.index_view),
-    path('about', views.about_view),
-    path('contact', views.contact_view),
+    path('about/', views.about_view),
+    path('contact/', views.contact_view),
 ]
