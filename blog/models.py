7d5a14e3c50adc9c9cae6a78ce085abd3b50533d
mobin <6ix.mobin@gmail.com>
2023-11-29 17:07:08 +0330

    models
    craeted contact model and edited post model

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,14 @@ from django.db import models
 
 
 class Post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # tag
+    # category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
