678b04506b67f393544e7ad841d35d0cea43164b
mobin <6ix.mobin@gmail.com>
2023-12-08 17:29:50 +0330

    update template
    filter show date and add include in project

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
 import datetime, time
-from datetime import datetime
+from django.utils import timezone
 from django.db.models import F
 
 
 def blog_view(request):
-    now1 = datetime.now()
+    now1=timezone.now()
     posts = Post.objects.filter(published_date__lt=now1, status=1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 
 def single_view(request, pid):
-    now = datetime.now()
+    now=timezone.now()
     posts = get_object_or_404(Post, published_date__lt=now, status=1, id=pid)
     posts.counted_view += 1
     posts.save()
